1a640ed10e406a10a0a14c240e977fa067076087
bason <basonsan@yandex.ru>
2020-10-15 00:23:03 +0400

    add block switcher
    завершил блок переключения товаров

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+//alert (1234);
+$(document).ready(function(){
+    $('.switching__img-wrapper').slick({
+        speed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/4_switching/arrow_left.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/4_switching/arrow_right.png" alt="right"></button>',
+    });
+  });
