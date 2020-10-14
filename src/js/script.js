1f4e52e83d97dc1d72631edcd0abc97dad815b29
bason <basonsan@yandex.ru>
2020-10-15 00:58:50 +0400

    adaptation switcher
    адаптировал блок слайдер под мобильные устройства

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,14 @@ $(document).ready(function(){
         speed: 2000,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/4_switching/arrow_left.png" alt="left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/4_switching/arrow_right.png" alt="right"></button>',
+        responsive: [
+            {
+              breakpoint: 950,
+              settings: {
+                arrows:false,
+                dots: true
+              }
+            }
+          ]
     });
   });
