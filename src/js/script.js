774e1412954843f241644a036f32ec5d83776f89
bason <basonsan@yandex.ru>
2020-10-16 00:17:09 +0400

    speed down
    уменьшил скорость переключения карусели

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 //alert (1234);
 $(document).ready(function(){
     $('.switching__img-wrapper').slick({
-        speed: 2000,
+        speed: 1000,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/4_switching/arrow_left.png" alt="left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/4_switching/arrow_right.png" alt="right"></button>',
         responsive: [
