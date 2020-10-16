6bcda094797e73d575a0482f46b642297d313f91
bason <basonsan@yandex.ru>
2020-10-17 02:00:52 +0400

    add product
    добавил раздел с товарами и адаптировал под мобильные устройства

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,23 @@ $(document).ready(function(){
             }
           ]
     });
+
+    $('ul.product__tabs').on('click', 'li:not(.product__tab_active)', function() {
+        $(this)
+          .addClass('product__tab_active').siblings().removeClass('product__tab_active')
+          .closest('div.container').find('div.product__items').removeClass('product__items_active').eq($(this).index()).addClass('product__items_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.card-product__descr-wrapper').eq(i).toggleClass('card-product__descr-wrapper_active');
+                //$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.card-product__link');
+    toggleSlide('.card-product__link-back');
   });
